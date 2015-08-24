611318dfd38d12fb1a934862024bf90410feb576
Anton Lapshin <antvallap@gmail.com>
2015-08-24 23:06:16 +0600

    Added oauth2 to launcher

--- deploy.bat
+++ deploy.bat
@@ -1,2 +1,2 @@
-"python" "C:\Program Files (x86)\Google\google_appengine\appcfg.py" update C:\projects\penalty\client\app
+"python" "C:\Program Files (x86)\Google\google_appengine\appcfg.py" --oauth2 update C:\projects\penalty\client\app
 PAUSE
