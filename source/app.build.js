33e89b99e69427b85877a84175db8f573f6e3827
Anton Lapshin <antvallap@gmail.com>
2016-12-07 21:28:59 +0100

    build

--- source/app.build.js
+++ source/app.build.js
@@ -12,7 +12,7 @@
     modules: [
         {
             name: "main",
-            include: ['components/choice/vm','components/game/vm','components/info/vm','components/instruction/vm','components/intro/vm','components/lang/vm','components/load/vm','components/match/vm','components/member/vm','components/psychic/vm','components/result/vm','components/scores/vm','components/sound/vm','components/team/vm','components/timer/vm','components/top/vm','components/tournament/vm','components/twist/vm']
+            include: ['components/choice/vm','components/game/vm','components/info/vm','components/instruction/vm','components/intro/vm','components/lang/vm','components/load/vm','components/match/vm','components/member/vm','components/psychic/vm','components/result/vm','components/scores/vm','components/sound/vm','components/team/vm','components/timer/vm','components/top/vm','components/tournament/vm','components/twist/vm', 'components/slideshowplanet/vm']
         }
     ]
 })
