1171942e27a2d06bd59a5981e88b3fdc81ab6275
Anton Lapshin <antvallap@gmail.com>
2016-12-18 11:25:51 +0100

    Updated url to landing page

--- source/project/www/components/slideshowplanet/vm.js
+++ source/project/www/components/slideshowplanet/vm.js
@@ -11,7 +11,7 @@ define([
         },
 
         click: function () {
-            var win = window.open('http://slideshowplanet.com', '_blank');
+            var win = window.open('http://slideshowplanet.com/landing', '_blank');
             win.focus();
         },
 
